038f8f14fa8f6f1a6beb2647e4d40952025f97c0
Zabeehullah <study.yadgar@gmail.com>
2026-03-22 23:53:37 -0400

    show exam and submit is worked

--- myproject/settings.py
+++ myproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 
 CSRF_TRUSTED_ORIGINS = ['https://*.cognitiveclass.ai']
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Application definition
 INSTALLED_APPS = [
     'onlinecourse.apps.OnlinecourseConfig',
